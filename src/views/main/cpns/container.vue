b67f4043078a302e54c473c0543aeb0d8361ccc2
Mac <545756792@qq.com>
2022-11-30 14:08:50 +0800

    新增路由切换过渡动画

--- src/views/main/cpns/container.vue
+++ src/views/main/cpns/container.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="route" appear mode="out-in">
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -9,4 +13,18 @@
 </script>
 
 <style scoped lang="less">
+.route-enter-from,
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+  transform: translateX(0px);
+}
+.route-enter-active,
+.route-leave-active {
+  transition:  all .4s;
+}
 </style>
